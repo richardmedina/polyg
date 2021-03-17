4fb44137e2c91457f49221d54f0d2f2f62bfe7d2
Richard <ricki9@gmail.com>
2021-03-16 23:15:56 -0700

    Added Test Controller

---
+++ src/Polyg/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Polyg.Models.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Polyg.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public async Task<IActionResult> GetTestResponse()
+        {
+            await Task.Yield();
+
+            return Ok(new TestResponse
+            {
+                HostName = Dns.GetHostName(),
+                Date = DateTime.Now
+            });
+        }
+    }
+}
