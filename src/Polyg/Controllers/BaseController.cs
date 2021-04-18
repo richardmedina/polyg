ca5e2d8059580c4ce07fde985ae6551c6f87cd5f
Richard <ricki9@gmail.com>
2021-04-17 23:34:39 -0700

    Update to NET 5.0 and Dockerfile update

--- src/Polyg/Controllers/BaseController.cs
+++ src/Polyg/Controllers/BaseController.cs
@@ -5,7 +5,6 @@ using Polyg.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 
 namespace Polyg.Controllers
